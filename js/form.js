0b398f8594b8b6e86ce6d35877a512caa203a4d4
adailtonasp <adailton-hag@hotmail.com>
2024-04-10 16:34:23 -0300

    adição da listagem de clientes

--- js/form.js
+++ js/form.js
@@ -11,11 +11,26 @@ function capturarDados(){
 
     const cJson = JSON.stringify(cliente);
 
-    localStorage.setItem(Date.now().toString(),cJson);
+    //o arquivo e convertido em json e salvo com uma chave de timestamp
+    localStorage.setItem(Date.now().toString,cJson);
 
-    // fs.writeFileSync('../assets/local/mensagens.json',cJson);
-    
     console.log("email",email);
     console.log("mensagem",mensagem);
 
 }
+
+function listarDados(){
+
+    console.log("listarDados");
+
+    for(let i = 0; i < localStorage.length;i++){
+        const cliente_json = localStorage.getItem(localStorage.key(i))
+        console.log(cliente_json);
+        if(cliente_json){
+            const cliente = JSON.parse(cliente_json)
+            document.getElementById('lista').innerText += `\nEmail: ${cliente.email} | Mensagem: ${cliente.mensagem} `
+        }
+    }
+
+    return lista;
+} 
